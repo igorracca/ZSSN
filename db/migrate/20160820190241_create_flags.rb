fb8a58e5c7afbe9743d4284b0f0eb1a28c9892eb
igorracca <igorracca13@gmail.com>
2016-08-21 15:49:48 -0300

    fixing migrate file

--- db/migrate/20160820190241_create_flags.rb
+++ db/migrate/20160820190241_create_flags.rb
@@ -2,6 +2,7 @@ class CreateFlags < ActiveRecord::Migration
   def change
     create_table :flags do |t|
       t.string :informer
+      t.string :infected
       t.references :survivor, index: true, foreign_key: true
 
       t.timestamps null: false
